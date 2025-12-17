602658be1997a56235d197f428a1c27900c1c095
guillaume legros <legrosguillaume@yahoo.fr>
2025-12-17 12:31:50 +0100

    add meta tag, add font

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Hacketweet</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Hacketweet description" />
       </Head>
       <Component {...pageProps} />
     </>
