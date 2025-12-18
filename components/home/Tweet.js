035dac57c59d66dc245326f3db45d7ee7332a4ae
guillaume legros <legrosguillaume@yahoo.fr>
2025-12-18 21:37:53 +0100

    render tweet componant and route post

--- components/home/Tweet.js
+++ components/home/Tweet.js
@@ -1,6 +1,34 @@
+import { useState, useEffect } from 'react'
 import styles from './Tweet.module.css'
 
 export default function Tweet() {
+  const [inputData, setInputData] = useState("");
+
+  function handleInput(e) {
+    setInputData(e.target.value)
+  }
+
+  async function handleTweet({ updateNewTweet }) {
+    if (!inputData.trim()) return;
+    if (!user.token) return; // depuis reducer
+
+    const response = await fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        token: 'xxxxxx',
+        content: inputData,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.result) {
+      setInputData('');
+      updateNewTweet();
+    }
+  }
+
   return (
     <div className={styles.tweetContainer}>
       <div className={styles.inputContainer}>
@@ -8,12 +36,20 @@ export default function Tweet() {
           type="text"
           placeholder="What's up?"
           className={styles.tweetInput}
+          value={inputData}
+          maxLength={280}
+          onChange={handleInput}
+
         />
       </div>
 
       <div className={styles.counterBtn}>
-        <p className={styles.charCounter}>0/280</p>
-        <button className={styles.tweetBtn}>Tweet</button>
+        <p className={styles.charCounter}>{inputData.length}/280</p>
+        <button
+          className={styles.tweetBtn}
+          onClick={handleTweet}
+          disabled={inputData.length === 0 || inputData.length > 280}
+        >Tweet</button>
       </div>
     </div>
 
