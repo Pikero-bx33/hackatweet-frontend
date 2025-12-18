8ffa80ee7f80ddfbc79fe45c9126fa5af35eb65a
guillaume legros <legrosguillaume@yahoo.fr>
2025-12-18 20:29:53 +0100

    render LastTweets component

--- components/home/LastTweets.js
+++ components/home/LastTweets.js
@@ -3,7 +3,25 @@ import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-export default function LastTweets() {
+
+function timeAgo(date) {
+  const now = new Date();
+  const createdDate = new Date(date);
+  const seconds = Math.floor((now - createdDate) / 1000);
+
+  if (seconds < 60) return `${seconds}s`;
+  const minutes = Math.floor(seconds / 60);
+  if (minutes < 60) return `${minutes}m`;
+  const hours = Math.floor(minutes / 60);
+  if (hours < 24) return `${hours}h`;
+  const days = Math.floor(hours / 24);
+  if (days < 7) return `${days}d`;
+  const weeks = Math.floor(days / 7);
+  return `${weeks}w`;
+};
+
+
+export default function LastTweets(props) {
   return (
     <div className={styles.lastTweetsContainer}>
       <div className={styles.userInfo}>
@@ -14,11 +32,11 @@ export default function LastTweets() {
           height={40}
           className={styles.userImage}
         />
-        <p className={styles.username}>Antoine</p>
-        <small>@AntoineLeProf</small>
-        <span className={styles.time}>- 5 hours</span>
+        <p className={styles.username}>{props.userId.fullName}</p>
+        <small>@{props.userId.username}</small>
+        <span className={styles.time}>- {timeAgo(props.createdAt)}</span>
       </div>
-      <p className={styles.tweetContent}>Welcome to <strong>#hackatweet</strong> guys 😎</p>
+      <p className={styles.tweetContent}>{props.content}</p>
       <div className={styles.heartContainer}>
         <FontAwesomeIcon
           icon={faHeart}
