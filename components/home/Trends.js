df6bd3e8984aa2fb58d2d03ca8026f51f181166e
guillaume legros <legrosguillaume@yahoo.fr>
2025-12-19 17:49:02 +0100

    create and display trends component

--- components/home/Trends.js
+++ components/home/Trends.js
@@ -1,9 +1,21 @@
-import styles from './Trends.module.css'
+import styles from './Trends.module.css';
+import Link from 'next/link';
 
 export default function Trends() {
   return (
     <div className={styles.trendsContainer}>
-
+      <div className={styles.trendsHashtagContainer}>
+        <Link href="/hashtagPage"><span className={styles.trendsHashtag}>#hackatweet</span></Link>
+        <span className={styles.tweetCounts}>2 tweets</span>
+      </div>
+      <div className={styles.trendsHashtagContainer}>
+        <Link href="/hashtagPage"><span className={styles.trendsHashtag}>#hackatweet</span></Link>
+        <span className={styles.tweetCounts}>2 tweets</span>
+      </div>
+      <div className={styles.trendsHashtagContainer}>
+        <Link href="/hashtagPage"><span className={styles.trendsHashtag}>#hackatweet</span></Link>
+        <span className={styles.tweetCounts}>2 tweets</span>
+      </div>
     </div>
   )
 }
